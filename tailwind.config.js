12a4cb95e0f85101c835f9bf276aa907c640e5b0
Rafael Almeida V <104868359+rafaelalmeidaV@users.noreply.github.com>
2023-09-18 12:38:04 -0300

    portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: { 
+        burtons: "burtons",
+
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':

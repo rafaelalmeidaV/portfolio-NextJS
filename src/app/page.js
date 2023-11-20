3bcb1121e386e60f811766dfac756565708220ce
Rafael Almeida V <104868359+rafaelalmeidaV@users.noreply.github.com>
2023-11-20 18:54:08 -0300

    update-portifolio

--- src/app/page.js
+++ src/app/page.js
@@ -66,7 +66,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      {/* <main>
         <section className="min-h-screen px-20">
           <header className='fixed top-0 left-0 right-0 flex flex-row p-8 justify-around border-transparent bg-zinc-800 bg-opacity-75'>
             <nav className='flex flex-row justify-center' >
@@ -174,7 +174,7 @@ export default function Home() {
                 </div>
               </div>
 
-              {/* Repita o mesmo padrão para outras divs */}
+              
 
             </div>
           </div>
@@ -208,7 +208,100 @@ export default function Home() {
 
 
 
+      </main> */}
+      <main >
+        <div className="flex justify-evenly pt-28">
+          <div >
+            <div><h2 className='text-5xl text-neutral-50 font-bold '>Rafael Almeida</h2></div>
+            <h3 className='text-2xl py-3'>Software Engineer</h3>
+            <p className='text-neutral-50 text-opacity-40'>
+              I build exceptional and accessible digital
+              <br />
+              experiences for the web and Mobile.
+            </p>
+            <div className='flex justify-start gap-6 py-3 text-sky-500'>
+              <Image
+                src={'/react-3.png'}
+                className="h-6 flex"
+                width={24}
+                height={24}
+              />
+              <SiJavascript className='text-yellow-400 bg-black mt-1 ml-1' />
+              <SiTypescript className='text-blue-600 bg-white mt-1 ml-1' />
+              <Image
+                src={'/Python-logo-notext.svg.png'}
+                className="h-6 flex"
+                width={24}
+                height={24}
+
+              />
+            </div>
+
+          </div>
+          <div>
+            <div className='max-w-lg text-neutral-50 text-opacity-40'>
+
+              <h1 >
+                At 20, I am a <span class=" text-white">software engineering</span> student with expertise in
+                <span class=" text-white"> React</span>, seeking opportunities to apply my knowledge in <span class=" text-white">software development</span>.
+                <br /><br />Driven by a <span class=" text-white">strong desire</span> to learn and grow, I believe challenges are opportunities to acquire new skills. Committed to contributing significantly to <span class=" text-white">software projects</span>, I aim to refine my skills as an <span class=" text-opacity-80">engineer</span> and <span class=" text-white">educator</span>.
+                <br /><br />Confident in my ability to add value to a team with <span class=" text-white">dedication</span>, a <span class=" text-white ">passion</span> for technology, and effective teaching, I look forward to embracing new challenges and building a <span class=" text-white ">successful career</span> in <span class=" text-white">software engineering</span>.
+              </h1>
+
+            </div>
+            <ul className='py-24'>
+              <li className='max-w-lg hover:bg-white hover:bg-opacity-10 hover:shadow-md hover:shadow-[#0f131f] h-36 rounded-lg transition-all hover:text-teal-300 hover:delay-150 '>
+                <div className='flex'>
+                  <div className=' ml-4 mt-4'>
+                    <div className=' border-[2px] border-stone-700 rounded-sm'>
+                      <Image
+                        src={'/mageGpt.png'}
+                        width={110}
+                        height={70}
+                        style={{ borderRadius: '10px' }}
+                      />
+                    </div>
+                  </div>
+                  <div className='ml-4 mt-4 '>
+                    <h1>MAGE-GPT: Gerador de RPG</h1>
+                    <p className='text-sm text-white text-opacity-50 max-w-xs'>Embark on a journey into a world of Dungeons & Dragons 5e adventures with the assistance of artificial intelligence.</p>
+                    <div className='flex'>
+                      <div className='max-w-min bg-teal-900 text-teal-400 px-2 my-1 rounded-lg text-xs '> React </div>
+                      <div className='max-w-min bg-teal-900 text-teal-400 px-2 my-1 rounded-lg text-xs mx-2'> TypeScript </div>
+                    </div>
+                  </div>
+
+                </div>
+              </li>
+
+              <li className='max-w-lg hover:bg-white hover:bg-opacity-10 hover:shadow-md hover:shadow-[#0f131f] h-36 rounded-lg transition-all hover:text-teal-300 hover:delay-150 mt-5'>
+                <div className='flex'>
+                  <div className=' ml-4 mt-4'>
+                    <div className=' border-[2px] border-stone-700 rounded-sm'>
+                      <Image
+                        src={'/mageGpt.png'}
+                        width={110}
+                        height={70}
+                        style={{ borderRadius: '10px' }}
+                      />
+                    </div>
+                  </div>
+                  <div className='ml-4 mt-4 '>
+                    <h1>MAGE-GPT: Gerador de RPG</h1>
+                    <p className='text-sm text-white text-opacity-50 max-w-xs'>Embark on a journey into a world of Dungeons & Dragons 5e adventures with the assistance of artificial intelligence.</p>
+                    <div className='flex'>
+                      <div className='max-w-min bg-teal-900 text-teal-400 px-2 my-1 rounded-lg text-xs '> React </div>
+                      <div className='max-w-min bg-teal-900 text-teal-400 px-2 my-1 rounded-lg text-xs mx-2'> TypeScript </div>
+                    </div>
+                  </div>
+
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
       </main>
+
       <section id='contact'>
         <div className="container">
           <h1 className="title text-xl">Contact</h1>
